83e7d5d0b199c04e730a07ac7979c860ade49e55
Onex558 <thomasfigy@icloud.com>
2026-07-27 09:29:27 -0500

    Create uraniumgame.js

---
+++ uraniumgame.js
@@ -0,0 +1,22 @@
+  const sources = [
+        {
+            name: "GN Math Zone",
+            type: "json",
+            json: "https://cdn.jsdelivr.net/gh/freebuisness/assets@main/zones.json",
+            cover: "https://cdn.jsdelivr.net/gh/freebuisness/covers@main",
+            html: "https://cdn.jsdelivr.net/gh/freebuisness/html@main"
+        },
+        {
+            name: "Sea Bean",
+            type: "json",
+            json: "https://cdn.jsdelivr.net/gh/sea-bean-unblocked/sde@main/zzz.json",
+            cover: "https://cdn.jsdelivr.net/gh/sea-bean-unblocked/Singlemile@main/Icon/",
+            html: "https://cdn.jsdelivr.net/gh/sea-bean-unblocked/Singlemile@main/games/"
+        },
+        {
+            name: "UGS Games",
+            type: "combined",
+            repos: ["sea-bean-unblocked/Folder-1", "sea-bean-unblocked/Folder-2", "sea-bean-unblocked/Folder-3"],
+            icon: "https://sea-math-official.neocities.org/Perfet.png"
+        }
+    ];
